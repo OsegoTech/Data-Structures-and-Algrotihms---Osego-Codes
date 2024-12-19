d0b833d8e9a847b14411e218963eeec6f741f6be
Osego Codes <cliffosego2000@gmail.com>
2024-12-19 19:09:36 +0300

    add array and string manipulation examples with time complexity analysis

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -1,2 +1,74 @@
 array = [1,2,3,4,5,6]
-print(array[2])
+print(array)
+
+# append or insert an element to the array - on avarage O(1) time complexity
+array.append(7)
+print(array)
+
+# insert an element at a specific index - O(n) time complexity
+array.insert(0, 0)
+print(array)
+
+# remove an element from the array - O(n) time complexity
+array.remove(0)
+print(array)
+
+# remove the last element from the array - O(1) time complexity
+array.pop()
+print(array)
+
+# remove an element at a specific index - O(n) time complexity
+array.pop(0)
+print(array)
+
+# find the index of an element in the array - O(n) time complexity
+print(array.index(4))
+
+# check if an element is in the array - O(n) time complexity
+print(4 in array)
+
+# sort the array - O(nlogn) time complexity
+array.sort()
+print(array)
+
+# reverse the array - O(n) time complexity
+array.reverse()
+print(array)
+
+# copy the array - O(n) time complexity
+array_copy = array.copy()
+print(array_copy)
+
+# clear the array - O(n) time complexity
+array.clear()
+print(array)
+
+# create an array with a specific size and default value
+array = [0] * 5
+print(array)
+
+# create an array with a specific size and default value
+array = [0 for _ in range(5)]
+print(array)
+
+# create an array with a specific size and default value
+array = [i for i in range(5)]
+print(array)
+
+# create an array with a specific size and default value
+
+array = [i for i in range(5) if i % 2 == 0]
+print(array)
+
+# create a 2D array
+array = [[0] * 5 for _ in range(5)]
+print(array)
+
+# create a 2D array
+array = [[0 for _ in range(5)] for _ in range(5)]
+print(array)
+
+# create a 2D array
+
+array = [[i for i in range(5)] for _ in range(5)]
+print(array)
---
+++ Strings/strings.py
@@ -0,0 +1,14 @@
+# append to end of a string - O(n) time complexity
+s= "Hello, World!"
+b = s + " Welcome to Python"
+print(b)
+
+# check if a substring is in a string - O(n) time complexity
+print("Hello" in s)
+
+
+# access different positions in a string - O(1) time complexity
+print(s[0])
+
+# length of a string - O(1) time complexity
+print(len(s))
