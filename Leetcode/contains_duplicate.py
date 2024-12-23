8b6988b7f77e7f72d5bed18f7cef139a17dde2b4
Osego Codes <cliffosego2000@gmail.com>
2024-12-24 01:36:47 +0300

    add alternative containsDuplicate implementation with hash set in Python

--- Leetcode/contains_duplicate.py
+++ Leetcode/contains_duplicate.py
@@ -57,3 +57,15 @@ class Solution(object):
     
 test4 = Solution()
 print(test4.containsDuplicate([1,2,3,4,5,6,7,8,9,10,10]))
+
+
+# import List from typing
+from typing import List
+
+class Solution5: #time complexity O(n)  space complexity O(n)
+    def cintains_duplicate(self, nums: List[int]) -> bool:
+        hash_set = set()
+        for num in nums:
+            if num in hash_set:
+                return True
+            hash_set.add(num)
